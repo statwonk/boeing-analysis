d3591f773e2439e81824176affecfbe5d0e6410d
Christopher Peters <cpeter9@gmail.com>
2024-03-15 19:50:57 -0500

    Compare Boeing and Airbus accident records

--- analysis.R
+++ analysis.R
@@ -20,5 +20,36 @@ d %>%
     ggplot(aes(date, n)) +
     geom_bar(stat = "identity")
 
+# Count accidents by make
+d %>% 
+    count(acft_make, sort = T)
 
-aircraft
+# Count accidents by make and quarter
+d %>%
+    count(acft_make,
+          accident_date = lubridate::floor_date(date, "quarter")) -> accidents_by_make
+
+# Fill in quarters with 0 to prevent common mistakes that can afflict analysis
+accidents_by_make %>%
+    group_by(acft_make) %>%
+    complete(accident_date = seq.POSIXt(min(accident_date), max(accident_date), by = "quarter"), 
+             fill = list(n = 0)) %>%
+    mutate(n_minus_avg = n - mean(n)) %>%
+    ungroup() -> accidents_by_make2
+
+accidents_by_make2 %>%
+    filter(acft_make %in% c("BOEING", "AIRBUS")) %>%
+    ggplot(aes(accident_date, n, color = acft_make)) +
+    geom_line() +
+    geom_point() +
+    geom_hline(yintercept = 0) +
+    ggthemes::scale_color_colorblind() +
+    geom_smooth(method = "gam",
+                formula = y ~ s(x, bs = "ps")) +
+    # coord_cartesian(ylim = c(0, 120)) +
+    theme_bw() +
+    scale_x_datetime(date_breaks = "1 year", date_labels = "%Y") +
+    theme(legend.position = "top")
+    
+    
+    
