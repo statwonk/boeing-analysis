1106cde21e16da5f2d6301083b3de5c490a0396e
Christopher Peters <cpeter9@gmail.com>
2024-03-15 19:34:12 -0500

    Minor clean up

--- analysis.R
+++ analysis.R
@@ -5,23 +5,17 @@ library(mdbr)
 # list tables
 mdb_tables("avall.mdb")
 # read events data
-read_mdb("avall.mdb", "dt_events") -> d
-
-d %>%
-    mutate_if(is.character, factor) %>%
-    skimr::skim()
-
-d %>%
-    mutate_if(is.character, factor) %>%
-    count(col_name)
+read_mdb("avall.mdb", "dt_events") -> events
+read_mdb("avall.mdb", "dt_aircraft") -> dt_aircraft
+read_mdb("avall.mdb", "aircraft") -> aircraft
 
 
-read_mdb("avall.mdb", "dt_aircraft") -> aircraft
-
-d %>%
+events %>%
     mutate(date = substr(ev_id, 1, 8),
            date = lubridate::parse_date_time2(date, "Ymd")) %>%
-    left_join(aircraft, by = "ev_id") %>%
+    left_join(aircraft, by = "ev_id") -> d
+
+d %>%
     count(date = lubridate::floor_date(date, "month")) %>%
     ggplot(aes(date, n)) +
     geom_bar(stat = "identity")
